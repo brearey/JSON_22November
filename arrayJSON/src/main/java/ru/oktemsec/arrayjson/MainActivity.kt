68c9ef0480f8112e072e27edcc91b98ba9d5f7d1
Brearey <brearey4@gmail.com>
2021-11-23 18:49:14 +0900

    added onclick

--- arrayJSON/src/main/java/ru/oktemsec/arrayjson/MainActivity.kt
+++ arrayJSON/src/main/java/ru/oktemsec/arrayjson/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.oktemsec.arrayjson
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.ListView
 import android.widget.SimpleAdapter
 import org.json.JSONArray
@@ -14,34 +15,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val listView: ListView = findViewById(R.id.listView)
+        val btnShow:Button = findViewById(R.id.btn)
+        btnShow.setOnClickListener {
+            val listView: ListView = findViewById(R.id.listView)
 
-        val rawJSON = "[{\"MemberID\":\"1\",\"Name\":\"Елена\",\"Tel\":\"4954876107\"}" + ",{\"MemberID\":\"2\",\"Name\":\"Сергей\",\"Tel\":\"4954780121\"}" + ",{\"MemberID\":\"3\",\"Name\":\"Витя\",\"Tel\":\"4954543211\"}]"
+            //val rawJSON = "[{\"MemberID\":\"1\",\"Name\":\"Елена\",\"Tel\":\"4954876107\"}" + ",{\"MemberID\":\"2\",\"Name\":\"Сергей\",\"Tel\":\"4954780121\"}" + ",{\"MemberID\":\"3\",\"Name\":\"Витя\",\"Tel\":\"4954543211\"}]"
+            val rawJSON = createJSON()
 
-        try {
-            val data: JSONArray = JSONArray(rawJSON)
-            val arrayList:ArrayList<HashMap<String, String>> = ArrayList()
+            try {
+                val data: JSONArray = rawJSON
+                val arrayList:ArrayList<HashMap<String, String>> = ArrayList()
 
-            for (i in 0 until data.length()) {
-                val jsonObject:JSONObject = data.getJSONObject(i)
-                val map = HashMap<String, String>()
-                map["MemberID"] = jsonObject.getString("MemberID")
-                map["Name"] = jsonObject.getString("Name")
-                map["Tel"] = jsonObject.getString("Tel")
+                for (i in 0 until data.length()) {
+                    val jsonObject:JSONObject = data.getJSONObject(i)
+                    val map = HashMap<String, String>()
+                    map["MemberID"] = jsonObject.getString("MemberID")
+                    map["Name"] = jsonObject.getString("Name")
+                    map["Tel"] = jsonObject.getString("Tel")
 
-                arrayList.add(map)
+                    arrayList.add(map)
+                }
+                val arrayStings:Array<String> = arrayOf("MemberID", "Name", "Tel")
+                val arrayInt:IntArray = intArrayOf(R.id.item_textViewMemverId, R.id.item_textViewName, R.id.item_textViewNumber)
+                val simpleAdapter:SimpleAdapter = SimpleAdapter(this, arrayList, R.layout.list_item, arrayStings, arrayInt)
+                listView.adapter = simpleAdapter
+
             }
-            val arrayStings:Array<String> = arrayOf("MemberID", "Name", "Tel")
-            val arrayInt:IntArray = intArrayOf(R.id.item_textViewMemverId, R.id.item_textViewName, R.id.item_textViewNumber)
-            val simpleAdapter:SimpleAdapter = SimpleAdapter(this, arrayList, R.layout.list_item, arrayStings, arrayInt)
-            listView.adapter = simpleAdapter
-
-        }
-        catch (e: JSONException) {
-            Log.d("brearey", e.message.toString())
+            catch (e: JSONException) {
+                Log.d("brearey", e.message.toString())
+            }
         }
     }
-
+    @Throws(JSONException::class)
     private fun createJSON(): JSONArray {
         val jsonArrayList: ArrayList<JSONObject> = ArrayList()
         /*** Ряд 1  */
